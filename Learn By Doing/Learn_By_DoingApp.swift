f1dd28b596d9dd1245c4982ca7cfcbe76aa38a8c
shagun-nickelfox <shagun.bansal@nickelfox.com>
2024-01-17 10:10:57 +0530

    Initial Commit

---
+++ Learn By Doing/Learn_By_DoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_By_DoingApp.swift
+//  Learn By Doing
+//
+//  Created by Nickelfox on 17/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Learn_By_DoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
